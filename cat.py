5a2c30675c8db8f8897cc6fb13537a4d0d5738a4
Zayaan Ahmed <sanaurasif2@gmail.com>
2026-06-02 12:51:01 +0600

    Resolved all

---
+++ cat.py
@@ -0,0 +1,16 @@
+import json
+import sys
+
+try:
+    file_name = sys.argv[1]
+except IndexError:
+    file_name = 'final.json'
+except Exception as e:
+    print(f"Error: {e}")
+    print("Usage: python cat.py [file_name]")
+    sys.exit()
+
+json_obj = json.load(open(file_name, 'r'))
+
+for num, account in enumerate(json_obj['accounts']):
+    print(f" [{num + 1}] {account['name']} - {account['username']} - {account['tg_id']} - {account['number']}")
